74c13b50c9ac54c7f409864b987c80840378fd69
Leon Anavi <leon.anavi@konsulko.com>
2016-07-21 18:55:35 +0300

    rvi_sota_client/docker: Update scripts
    Update scripts and configuration files following
    the rename of sota_client to ota_plus_client.
    
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

--- rvi_sota_client/docker/run.sh
+++ rvi_sota_client/docker/run.sh
@@ -13,4 +13,4 @@ SOTA_CLIENT="${SOTA_CLIENT_ADDR:-0.0.0.0}:${SOTA_CLIENT_PORT:-9080}"
 RVI="${RVI:-http://rvi-client:8901}"
 
 export RUST_LOG=${RUST_LOG:-"sota_client=$LOGLEVEL"}
-/usr/bin/sota_client -c /var/sota/client.toml -r "$RVI" -e "$SOTA_CLIENT"
+/usr/bin/ota_plus_client --config /var/sota/ota.toml
